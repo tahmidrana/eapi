3211c37cc38e79835ae7f621a84346cb7a503e15
tahmidrana <tahmidrana@gmail.com>
2018-09-02 00:33:09 +0600

    eapi project add to github

---
+++ app/Model/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function reviews() {
+        return $this->hasMany('App\Model\Review');
+    }
+}
